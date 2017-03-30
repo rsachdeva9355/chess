8eef0c3c04cc42285da23043a97f4e91f0aca8a5
Deepak Gupta <gudee@amazon.com>
2017-03-30 21:37:41 +0100

    Add interface for move and positioned piece.

---
+++ src/main/java/model/Move.java
@@ -0,0 +1,24 @@
+package model;
+
+import model.cells.Cell;
+
+/**
+ * Represents a move. It contains the destination cell and the positioned piece to move.
+ */
+public interface Move {
+
+    /**
+     * Gets the piece to be moved.
+     *
+     * @return  The postioned piece to be moved.
+     */
+    public PositionedPiece getPiece();
+
+    /**
+     * Gets the cell to move to.
+     *
+     * @return  The cell to move the piece to.
+     */
+    public Cell getDestinationPosition();
+
+}
---
+++ src/main/java/model/PositionedPiece.java
@@ -0,0 +1,33 @@
+package model;
+
+import model.cells.Cell;
+import model.pieces.Piece;
+
+import java.util.List;
+
+/**
+ * Represents a positioned piece. It is composed of a cell and piece object.
+ */
+public interface PositionedPiece {
+
+    /**
+     * Gets all the possible moves assuming that there is no other piece on the board.
+     *
+     * @return  List of moves represented object can make.
+     */
+    public List<Move> getMoves();
+
+    /**
+     * Gets the position of the piece.
+     *
+     * @return  the cell on which piece is positioned.
+     */
+    public Cell getPosition();
+
+    /**
+     * Gets the positioned piece.
+     *
+     * @return the positioned piece.
+     */
+    public Piece getPiece();
+}
